60bdb55b0b1b3a3eaf617837779faac6799b573c
Kuswanth pitani <kushi.peethani222@gmail.com>
2025-06-26 13:55:01 +0530

    Add files via upload

---
+++ Python Password Generator.py
@@ -0,0 +1,18 @@
+import random
+import string
+
+def generate_password(length):
+    if length < 4:
+        return "Password length should be at least 4 characters."
+
+    characters = string.ascii_letters + string.digits + string.punctuation
+    password = ''.join(random.choice(characters) for _ in range(length))
+    return password
+
+# User input
+try:
+    user_length = int(input("Enter desired password length: "))
+    password = generate_password(user_length)
+    print("Generated Password:", password)
+except ValueError:
+    print("Please enter a valid number.")
